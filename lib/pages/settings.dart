68a31f576b76298b899b5442901700760debefaf
harunon <harunonsystem@gmail.com>
2020-06-12 17:14:36 +0900

    feat: settings-ui

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Setting'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: const Text('settings'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Setting'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: SettingsList(
+            sections: [
+              SettingsSection(
+                title: 'Section',
+                tiles: [
+                  SettingsTile(
+                    title: 'Language',
+                    subtitle: 'English',
+                    leading: Icon(Icons.language),
+                    onTap: () {},
+                  ),
+                  SettingsTile.switchTile(
+                    title: 'Use fingerprint',
+                    leading: Icon(Icons.fingerprint),
+                    switchValue: true,
+                    onToggle: (bool value) {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
